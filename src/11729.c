79b6927b757abb9ce0e46126e9b86a800c7040fb
skkuYJ <50133281+skkuYJ@users.noreply.github.com>
2020-10-18 04:52:10 +0900

    Update 11729.c

--- src/11729.c
+++ src/11729.c
@@ -1,38 +1,37 @@
 #include <stdio.h>
 #pragma warning(disable:4996)
 //미완성
-int n, * i, * j, * k, x, y, z,list[25][2];
-*i = &x;
-*j = &y;
-*k = &z;
+int n;
+#define MAX 25
+int stack1[MAX] , stack2[MAX], stack3[MAX];
+int top1, top2, top3;
 
-int hanoi(int n,int **list) {
-   
-    if (n == 1){
-        *list[0] = 1;
-        *list[1] = 3;
+int hanoi(int n) {
+
+    if (n == 1) {
+       
         return 1;
     }
     else
     {
-        return hanoi(n - 1,list) * 2 + 1;
+        return hanoi(n - 1) * 2 + 1;
     }
 }
-        
+
 
 int main()
 {
     int result;
     scanf("%d", &n);
 
-    result = hanoi(n, list);
+    result = hanoi(n);
     printf("%d\n", result);
-    printf("%d %d", *list[0], *list[1]);
+    printf("%d %d", list[0][0], list[0][1]);
     /*for (int i = 0; i < n; i++) {
         printf("%d %d\n", list[i][0], list[i][1]);
     }*/
 
-    
+
 
     return 0;
 }
