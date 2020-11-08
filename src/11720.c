c24fb7f724689daaf8eac0c2232f580ebbfd6518
skkuYJ <50133281+skkuYJ@users.noreply.github.com>
2020-11-08 19:15:29 +0900

    Update 11720.c

--- src/11720.c
+++ src/11720.c
@@ -1,3 +1,31 @@
+/* N개의 숫자가 공백없이 입력됐을때 숫자의 합을 출력
+첫째줄에 숫자의 개수 N, 그 다음줄 부터 숫자 N개가 공백없이 입력
+
+ex1.
+input
+1
+1
+
+output
+1
+
+ex2.
+input
+5
+54321
+
+output
+15
+
+ex3.
+input
+25
+7000000000000000000000000
+
+output
+7
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #pragma warning(disable:4996)
