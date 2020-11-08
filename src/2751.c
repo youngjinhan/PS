dc033001cb6ee0ee18773ac1c14ec76408b764d0
skkuYJ <50133281+skkuYJ@users.noreply.github.com>
2020-11-08 19:20:19 +0900

    Update 2751.c

--- src/2751.c
+++ src/2751.c
@@ -1,3 +1,26 @@
+// N개의 수 정렬하기 2 - 오름차순
+// 1<=N<=1,000,000
+// 첫째줄에 개수 N개, 두번째 줄부터 숫자 N개가 주어짐
+
+/*
+ex.
+input
+5
+5
+4
+3
+2
+1
+
+output
+1
+2
+3
+4
+5
+*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #pragma warning(disable:4996)
