30cc579af8e7c1ae4dd033842ecb28ab52845f3b
skkuYJ <50133281+skkuYJ@users.noreply.github.com>
2020-11-18 21:47:23 +0900

    Create 15651.c

---
+++ src/15651.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int out[10];
+int N, M;
+
+/*
+int isGood(int cur)
+{
+	for (int i = 1; i < cur; i++)
+	{
+		if (out[cur] <= out[i])
+			return 0;
+	}
+	return 1;
+}
+*/
+
+void sol(int cur)
+{
+	if (cur > M)
+	{
+		for (int i = 1; i <= M; i++)
+			printf("%d ", out[i]);
+		printf("\n");
+		return;
+	}
+
+	for (int i = 1; i <= N; i++)
+	{
+		out[cur] = i;
+		sol(cur + 1);
+				
+	}
+	//printf("\n");
+	
+}
+
+
+int main()
+{
+	
+	scanf("%d %d", &N, &M);
+	sol(1);
+	return 0;
+
+}
+
+
