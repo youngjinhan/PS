493212727c9156cddfe47656dfbe90e45b43ed26
skkuYJ <50133281+skkuYJ@users.noreply.github.com>
2020-12-27 19:39:20 +0900

    Update 11653.c

--- src/11653.c
+++ src/11653.c
@@ -1,3 +1,44 @@
+/*
+정수 N을 소인수분해하기.
+첫째 줄에 정수 N (1 ≤ N ≤ 10,000,000)이 주어진다.
+N의 소인수분해 결과를 한 줄에 하나씩 오름차순으로 출력한다. N이 1인 경우 아무것도 출력하지 않는다.
+
+ex1.
+input
+72
+
+output
+2
+2
+2
+3
+3
+
+ex2.
+input
+3
+
+output
+3
+
+ex3.
+input
+6
+
+output
+2
+3
+
+ex4.
+input
+9991
+
+output
+97
+103
+
+*/
+
 #include <stdio.h>
 
 int main()
