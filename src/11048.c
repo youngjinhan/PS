ed1f6986fb57fb4ef862f2421090d8206f1212b4
skkuYJ <50133281+skkuYJ@users.noreply.github.com>
2021-05-01 03:13:18 +0900

    Create 11048.c

---
+++ src/11048.c
@@ -0,0 +1,42 @@
+// DP 문제인듯 하여 DP로 풀었으나 시간초과
+// LCS로 해결
+
+#include <stdio.h>
+int candy[1001][1001];
+
+int main()
+{
+	
+	int N, M;
+	scanf("%d %d", &N, &M);
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			scanf("%d", &candy[i][j]);
+		}
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			if (i == 0 && j == 0)
+				continue;
+			if (i == 0)
+				candy[i][j] += candy[i][j - 1];
+			else if (j == 0)
+				candy[i][j] += candy[i - 1][j];
+			else
+			{
+				if (candy[i - 1][j] > candy[i][j - 1])
+					candy[i][j] += candy[i - 1][j];
+				else
+					candy[i][j] += candy[i][j - 1];
+			}
+		}
+	}
+	printf("%d\n", candy[N - 1][M - 1]);
+
+	return 0;
+}
