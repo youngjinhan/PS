b86d0219b6491dbc6815140fc83c34b7cc85d263
skkuYJ <50133281+skkuYJ@users.noreply.github.com>
2021-05-15 15:43:45 +0900

    Update 2309.py

--- src/2309.py
+++ src/2309.py
@@ -8,6 +8,7 @@ for i in range(9):
         if i>=j or i>=len(small) or j>=len(small):
             continue
         if small[i]+small[j]==fake_sum:
+            # 제거할때 small[j]부터 뺴야함. i<j이기 때문에 small[i] 부터 빼면 j가 한칸 앞으로 땡겨지기 때문에 small[j-1]에 값이 저장.
             small.remove(small[j])
             small.remove(small[i])
             fake_sum=0
