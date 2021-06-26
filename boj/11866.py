6d3b935638698dc6922050285114391d560d148a
skkuYJ <50133281+skkuYJ@users.noreply.github.com>
2021-06-27 02:19:17 +0900

    Update 11866.py

--- boj/11866.py
+++ boj/11866.py
@@ -1,15 +1,24 @@
 N,K=map(int,input().split())
 circle=[i for i in range(1,N+1)]
-eliminated=[]
+result=[]
 
 i=0
-j=N
-while len(eliminated)<N:
-    eliminated.append(circle[(i+K-1)%j])
+
+# while loop ver.
+'''
+while j!=0:
+    result.append(circle[(i+K-1)%j])
     del circle[(i+K-1)%j]
     i=(i+K-1)%j
     j-=1
+'''
+
+# for loop ver.
+for j in range(N,0,-1):
+    result.append(circle[(i+K-1)%j])
+    del circle[(i+K-1)%j]
+    i=(i+K-1)%j
     
 print('<',end='')
-print(*eliminated, sep=', ', end='')
+print(*result, sep=', ', end='')
 print('>')
