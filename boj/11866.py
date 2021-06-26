c390f080e84e4092574124fec9a4b781fba8955f
skkuYJ <50133281+skkuYJ@users.noreply.github.com>
2021-06-27 02:27:25 +0900

    Update 11866.py

--- boj/11866.py
+++ boj/11866.py
@@ -18,7 +18,23 @@ for j in range(N,0,-1):
     result.append(circle[(i+K-1)%j])
     del circle[(i+K-1)%j]
     i=(i+K-1)%j
-    
+
+# stay the length of original input list
+# time complexity is worst of the 3 ways
+'''
+cur=0
+while len(result)<N:
+    if circle[i]!=0:
+        cur+=1
+        if cur==K:
+            result.append(circle[i])
+            circle[i]=0
+            cur=0
+    i=(i+1)%N
+'''
+
 print('<',end='')
 print(*result, sep=', ', end='')
 print('>')
+
+
