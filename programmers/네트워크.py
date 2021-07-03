8c06ee5255704c4ff22a74a662299a84970ada1d
skkuYJ <50133281+skkuYJ@users.noreply.github.com>
2021-07-04 02:31:55 +0900

    Update 네트워크.py

--- programmers/네트워크.py
+++ programmers/네트워크.py
@@ -1,3 +1,23 @@
+# 내 풀이
+check=[]
+def connect(node,n,computers):
+    check.append(node)
+    
+    for i in range(n):
+        if computers[node][i]==1 and i not in check:
+            connect(i,n,computers)
+
+def solution(n, computers):
+    answer = 0
+    for i in range(n):
+        if i not in check:
+            connect(i,n,computers)
+            answer+=1
+        
+    return answer
+
+
+# 인터넷 검색한 풀이
 def solution(n, computers):
     answer = 0
     visited = [False for i in range(n)]
