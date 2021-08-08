ead2d6171c980c7f4228d3e2f6e8bfa4d3ae7c5a
youngjinhan <50133281+youngjinhan@users.noreply.github.com>
2021-08-08 16:00:44 +0900

    add another case in 3273.py

--- boj/3273.py
+++ boj/3273.py
@@ -18,3 +18,14 @@ while left<right:
         left+=1
 
 print(count)
+
+# 더 나은 풀이
+'''
+n=int(input())
+a = {*map(int, input().split())}
+x=int(input())
+
+count=sum((x-i in a) for i in a)//2
+
+print(count)
+'''
