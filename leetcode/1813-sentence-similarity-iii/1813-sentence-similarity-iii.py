31b24be3d0e3e6b259372341401ffd23dcd11201
youngjinhan <50133281+youngjinhan@users.noreply.github.com>
2024-10-07 04:43:13 +0900

    Time: 21 ms (99.38%), Space: 16.5 MB (80.31%) - LeetHub

--- leetcode/1813-sentence-similarity-iii/1813-sentence-similarity-iii.py
+++ leetcode/1813-sentence-similarity-iii/1813-sentence-similarity-iii.py
@@ -1,5 +1,36 @@
 class Solution:
     def areSentencesSimilar(self, sentence1: str, sentence2: str) -> bool:
+        '''
+        good solution
+        https://leetcode.com/problems/sentence-similarity-iii/solutions/5875276/step-by-step-guide-to-cracking-sentence-similarity
+        '''
+        # Split the sentences into words
+        words1 = sentence1.split()
+        words2 = sentence2.split()
+
+        # Ensure words1 is the longer sentence
+        if len(words1) < len(words2):
+            words1, words2 = words2, words1
+        
+        start, end = 0, 0
+        n1, n2 = len(words1), len(words2)
+        
+        # Compare from the start
+        while start < n2 and words1[start] == words2[start]:
+            start += 1
+        
+        # Compare from the end
+        while end < n2 and words1[n1 - end - 1] == words2[n2 - end - 1]:
+            end += 1
+        
+        # Check if the remaining unmatched part is in the middle
+        return start + end >= n2
+
+
+        '''
+        my first solution
+
+
         def check():
             i, j = 1, 1
             while j < len(sep2) and sep1[i] == sep2[j]:
@@ -52,3 +83,5 @@ class Solution:
             sep1 = sep1[::-1]
             sep2 = sep2[::-1]
             return check()
+        '''
+
