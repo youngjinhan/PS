7c8c58a9b182ec5019065dc2e6ddc26609dceeb4
youngjinhan <50133281+youngjinhan@users.noreply.github.com>
2024-10-11 22:18:37 +0900

    Time: 404 ms (35.14%), Space: 17.5 MB (78.38%) - LeetHub

--- leetcode/0962-maximum-width-ramp/0962-maximum-width-ramp.py
+++ leetcode/0962-maximum-width-ramp/0962-maximum-width-ramp.py
@@ -1,7 +1,36 @@
-class Solution:
-    def maxWidthRamp(self, nums: List[int]) -> int:
-        # 직접 풀지 못한 문제
-        # 해답 참고
+class Solution(object):
+    def maxWidthRamp(self, nums):
+        '''
+        Solution 
+        O(n)
+        '''
+        n = len(nums)
+        right_max = [None] * n
+
+        # Fill right_max array with the maximum values from the right
+        right_max[n - 1] = nums[n - 1]
+        for i in range(n - 2, -1, -1):
+            right_max[i] = max(right_max[i + 1], nums[i])
+
+        left = 0
+        right = 0
+        max_width = 0
+
+        # Traverse the array using left and right pointers
+        while right < n:
+            # Move left pointer forward if current left exceeds right_max
+            while left < right and nums[left] > right_max[right]:
+                left += 1
+            max_width = max(max_width, right - left)
+            right += 1
+
+        return max_width
+
+
+        '''
+        Solution 
+        O(nlogn)
+        '''
         n = len(nums)
         indices = [i for i in range(n)]
 
@@ -17,5 +46,3 @@ class Solution:
             min_index = min(min_index, i)
 
         return max_width
-        
-
