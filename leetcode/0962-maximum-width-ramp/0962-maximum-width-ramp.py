079168189372a11f50d7015ee0cfb8b2e5c9e9e8
youngjinhan <50133281+youngjinhan@users.noreply.github.com>
2024-10-12 22:01:07 +0900

    Time: 397 ms (35.2%), Space: 18.3 MB (26.62%) - LeetHub

--- leetcode/0962-maximum-width-ramp/0962-maximum-width-ramp.py
+++ leetcode/0962-maximum-width-ramp/0962-maximum-width-ramp.py
@@ -1,5 +1,27 @@
 class Solution(object):
     def maxWidthRamp(self, nums):
+        '''
+        Monotonic Decreasing Stack
+        '''
+        indices_stack = []
+        n = len(nums)
+        ans = 0
+        for i in range(n):
+            if not indices_stack or nums[indices_stack[-1]] >= nums[i]:
+                indices_stack.append(i)
+        
+        for j in range(n-1, -1, -1):
+            if not indices_stack:
+                    break
+            while indices_stack and nums[indices_stack[-1]] <= nums[j]:
+                ans = max(ans, j-indices_stack[-1])
+                indices_stack.pop()
+        return ans
+
+        '''
+        Two Pointers
+        Time Complexity : O(n)
+        '''
         n = len(nums)
         part_max_list = [nums[n-1]] * n
         
