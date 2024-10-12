b11d8bfb6a8c77f3227f8d90c7b48d7b5eda4c38
youngjinhan <50133281+youngjinhan@users.noreply.github.com>
2024-10-12 19:23:02 +0900

    Time: 400 ms (32.22%), Space: 17.6 MB (93.17%) - LeetHub

--- leetcode/0962-maximum-width-ramp/0962-maximum-width-ramp.py
+++ leetcode/0962-maximum-width-ramp/0962-maximum-width-ramp.py
@@ -1,48 +1,16 @@
 class Solution(object):
     def maxWidthRamp(self, nums):
-        '''
-        Solution 
-        O(n)
-        '''
         n = len(nums)
-        right_max = [None] * n
+        part_max_list = [nums[n-1]] * n
+        
+        for i in range(n-2, -1, -1):
+            part_max_list[i] = max(nums[i], part_max_list[i+1])
 
-        # Fill right_max array with the maximum values from the right
-        right_max[n - 1] = nums[n - 1]
-        for i in range(n - 2, -1, -1):
-            right_max[i] = max(right_max[i + 1], nums[i])
-
-        left = 0
-        right = 0
-        max_width = 0
-
-        # Traverse the array using left and right pointers
+        left, right, ans = 0, 0, -1
         while right < n:
-            # Move left pointer forward if current left exceeds right_max
-            while left < right and nums[left] > right_max[right]:
+            while left < right and nums[left] > part_max_list[right]:
                 left += 1
-            max_width = max(max_width, right - left)
+            ans = max(ans, right-left)
             right += 1
 
-        return max_width
-
-
-        '''
-        Solution 
-        O(nlogn)
-        '''
-        n = len(nums)
-        indices = [i for i in range(n)]
-
-        # Sort indices based on corresponding values in nums and ensure stability
-        indices.sort(key=lambda i: (nums[i], i))
-
-        min_index = n  # Minimum index encountered so far
-        max_width = 0
-
-        # Calculate maximum width ramp
-        for i in indices:
-            max_width = max(max_width, i - min_index)
-            min_index = min(min_index, i)
-
-        return max_width
+        return ans
