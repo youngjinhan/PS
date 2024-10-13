6d03a8abf8d7c9510c5d9aa44c25fad4cf0209d7
youngjinhan <50133281+youngjinhan@users.noreply.github.com>
2024-10-13 14:08:43 +0900

    Time: 433 ms (16.26%), Space: 26.4 MB (14.88%) - LeetHub

--- leetcode/0962-maximum-width-ramp/0962-maximum-width-ramp.py
+++ leetcode/0962-maximum-width-ramp/0962-maximum-width-ramp.py
@@ -1,38 +1,21 @@
-class Solution(object):
-    def maxWidthRamp(self, nums):
-        '''
-        Monotonic Decreasing Stack
-        '''
-        indices_stack = []
+class Solution:
+    def maxWidthRamp(self, nums: List[int]) -> int:
+        # 직접 풀지 못한 문제
+        # 해답 참고
         n = len(nums)
-        ans = 0
-        for i in range(n):
-            if not indices_stack or nums[indices_stack[-1]] >= nums[i]:
-                indices_stack.append(i)
+        indices = [i for i in range(n)]
+
+        # Sort indices based on corresponding values in nums and ensure stability
+        indices.sort(key=lambda i: (nums[i], i))
+
+        min_index = n  # Minimum index encountered so far
+        max_width = 0
+
+        # Calculate maximum width ramp
+        for i in indices:
+            max_width = max(max_width, i - min_index)
+            min_index = min(min_index, i)
+
+        return max_width
         
-        for j in range(n-1, -1, -1):
-            if not indices_stack:
-                    break
-            while indices_stack and nums[indices_stack[-1]] <= nums[j]:
-                ans = max(ans, j-indices_stack[-1])
-                indices_stack.pop()
-        return ans
 
-        '''
-        Two Pointers
-        Time Complexity : O(n)
-        '''
-        n = len(nums)
-        part_max_list = [nums[n-1]] * n
-        
-        for i in range(n-2, -1, -1):
-            part_max_list[i] = max(nums[i], part_max_list[i+1])
-
-        left, right, ans = 0, 0, 0
-        while right < n:
-            while left < right and nums[left] > part_max_list[right]:
-                left += 1
-            ans = max(ans, right-left)
-            right += 1
-
-        return ans
