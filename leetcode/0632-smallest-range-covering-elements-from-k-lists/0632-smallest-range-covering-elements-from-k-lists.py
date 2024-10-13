cf81e3a15ad5ff760686c34bd3b5d843f7fddd65
youngjinhan <50133281+youngjinhan@users.noreply.github.com>
2024-10-13 23:04:17 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ leetcode/0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.py
@@ -0,0 +1,39 @@
+class Solution:
+    def smallestRange(self, nums: List[List[int]]) -> List[int]:
+        '''
+        Editorial 풀이
+        '''
+        k = len(nums)
+        # Stores the current index of each list
+        indices = [0] * k
+        # To track the smallest range
+        range_list = [0, float("inf")]
+
+        while True:
+            cur_min, cur_max = float("inf"), float("-inf")
+            min_list_index = 0
+
+            # Find the current minimum and maximum values across the lists
+            for i in range(k):
+                current_element = nums[i][indices[i]]
+
+                # Update the current minimum
+                if current_element < cur_min:
+                    cur_min = current_element
+                    min_list_index = i
+
+                # Update the current maximum
+                if current_element > cur_max:
+                    cur_max = current_element
+
+            # Update the range if a smaller one is found
+            if cur_max - cur_min < range_list[1] - range_list[0]:
+                range_list[0] = cur_min
+                range_list[1] = cur_max
+
+            # Move to the next element in the list that had the minimum value
+            indices[min_list_index] += 1
+            if indices[min_list_index] == len(nums[min_list_index]):
+                break
+
+        return range_list
