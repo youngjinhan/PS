d285746f46787d5a761929fc11574f7aa25bf0a2
youngjinhan <50133281+youngjinhan@users.noreply.github.com>
2024-10-15 03:42:40 +0900

    Time: 548 ms (73.67%), Space: 23 MB (25.29%) - LeetHub

--- leetcode/1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.py
+++ leetcode/1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.py
@@ -1,6 +1,44 @@
 class Solution:
     def smallestChair(self, times: List[List[int]], targetFriend: int) -> int:
         '''
+        Set with Sorted Insertion
+        Time Complexity: O(nlogn)
+        '''
+        times = sorted(
+            [
+                (arrival, leave, index)
+                for index, (arrival, leave) in enumerate(times)
+            ]
+        )
+
+        next_chair = 0
+        available_chairs = []
+        leaving_queue = []
+
+        for time in times:
+            arrival, leave, index = time
+
+            # Free up chairs based on current time
+            while leaving_queue and leaving_queue[0][0] <= arrival:
+                _, chair = heapq.heappop(leaving_queue)
+                heapq.heappush(available_chairs, chair)
+
+            if available_chairs:
+                current_chair = heapq.heappop(available_chairs)
+            else:
+                current_chair = next_chair
+                next_chair += 1
+
+            # Check if it's the target friend
+            if index == targetFriend:
+                return current_chair
+
+            # Push current leave time and chair
+            heapq.heappush(leaving_queue, (leave, current_chair))
+
+        return 0
+
+        '''
         Event-based with Two Priority Queues
         Time Complexity: O(nlogn)
         '''
