bd3caaf1ca854da28ffbeb6b4fd3e67ad5f5ee38
youngjinhan <50133281+youngjinhan@users.noreply.github.com>
2024-11-02 11:45:07 +0900

    Time: 1 ms (96.81%), Space: 42.1 MB (25.89%) - LeetHub

--- leetcode/2490-circular-sentence/2490-circular-sentence.java
+++ leetcode/2490-circular-sentence/2490-circular-sentence.java
@@ -1,5 +1,22 @@
 class Solution {
     public boolean isCircularSentence(String sentence) {
+        /** 
+        Solution - Space-optimized Approach
+        Time Complexity: O(n)
+        Space Complexity: O(1)
+        */
+        int n = sentence.length();
+        for (int i=0; i<n; i++) {
+            if (sentence.charAt(i) == ' ' && sentence.charAt(i-1) != sentence.charAt(i+1)) return false;
+        }
+        return sentence.charAt(0) == sentence.charAt(n-1);
+
+
+        /**
+        My First Solution
+        Time Complexity: O(n)
+        Space Complexity: O(n)
+
         String[] words = sentence.split(" ");
         int n = words.length;
         for (int i = 0; i < n; i++) {
@@ -7,5 +24,7 @@ class Solution {
                 return false;
         }
         return true;
+        
+        */
     }
 }
