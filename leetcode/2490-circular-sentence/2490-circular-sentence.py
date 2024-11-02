855375e005b99cc3844176b1c44920e0b33760d0
youngjinhan <50133281+youngjinhan@users.noreply.github.com>
2024-11-02 11:26:17 +0900

    Time: 3 ms (12.78%), Space: 16.5 MB (45.68%) - LeetHub

--- leetcode/2490-circular-sentence/2490-circular-sentence.py
+++ leetcode/2490-circular-sentence/2490-circular-sentence.py
@@ -1,5 +1,16 @@
 class Solution:
     def isCircularSentence(self, sentence: str) -> bool:
+        '''
+        Solution with O(1) Space complexity
+        '''
+        for i in range(len(sentence)):
+            if sentence[i] == " " and sentence[i - 1] != sentence[i + 1]:
+                return False
+        return sentence[0] == sentence[len(sentence) - 1]
+
+        '''
+        My Solution
+        '''
         words = sentence.split()
         n = len(words)
         cur = 0
