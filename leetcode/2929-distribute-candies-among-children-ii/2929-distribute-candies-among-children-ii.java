49794a5c0bbb90fc35e6e60d22b6e3464b4cf568
youngjinhan <50133281+youngjinhan@users.noreply.github.com>
2025-06-01 21:20:03 +0900

    Time: 28 ms (10.29%), Space: 40.8 MB (82.35%) - LeetHub

--- leetcode/2929-distribute-candies-among-children-ii/2929-distribute-candies-among-children-ii.java
+++ leetcode/2929-distribute-candies-among-children-ii/2929-distribute-candies-among-children-ii.java
@@ -1,6 +1,6 @@
 class Solution {
     public long distributeCandies(int n, int limit) {
-        long ans = 0L;
+        long ans = 0;
 
         for (int i = 0; i <= Math.min(n, limit); i++) {
             ans += Math.max(Math.min(limit, n-i) - Math.max(0, n-i-limit) + 1, 0);
