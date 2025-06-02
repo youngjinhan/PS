2bc95b96ca5aee139f118a25b1ed7468a11f3fc6
youngjinhan <50133281+youngjinhan@users.noreply.github.com>
2025-06-02 18:49:20 +0900

    Time: 4 ms (26.17%), Space: 46 MB (34.39%) - LeetHub

--- leetcode/0135-candy/0135-candy.java
+++ leetcode/0135-candy/0135-candy.java
@@ -1,6 +1,7 @@
 class Solution {
     public int candy(int[] ratings) {
         int[] candies = new int[ratings.length];
+        int cnt = 0;
         Arrays.fill(candies, 1);
         for (int i = 0; i< candies.length - 1; i++) {
             if (ratings[i] > ratings[i+1] && candies[i] <= candies[i+1]) {
@@ -16,12 +17,9 @@ class Solution {
             } else if (ratings[i] < ratings[i-1] && candies[i] >= candies[i-1]) {
                 candies[i-1] = candies[i] + 1;
             }
+            cnt += candies[i];
         }
-    
-        // for (int i = 0; i < candies.length; i++) {
-        //     System.out.println(candies[i]);
-        // }
 
-        return Arrays.stream(candies).sum();
+        return cnt + candies[0];
     }
 }
