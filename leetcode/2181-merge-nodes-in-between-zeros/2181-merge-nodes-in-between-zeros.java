2744a27faab7d495b274674959896e77c520cbdf
youngjinhan <50133281+youngjinhan@users.noreply.github.com>
2025-06-29 21:29:21 +0900

    Time: 5 ms (81.79%), Space: 83.9 MB (17.8%) - LeetHub

---
+++ leetcode/2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeNodes(ListNode head) {
+        ListNode answer = null;
+        ListNode ans = answer;
+        ListNode cur = head;
+        int s = 0;
+        while (cur.next != null) {
+            cur = cur.next;
+            if (cur.val == 0) {
+                if (ans == null) {
+                    answer = new ListNode(s);
+                    ans = answer;
+                } else {
+                ans.next = new ListNode(s);
+                ans = ans.next;
+                }
+                s = 0;
+                continue;
+            }
+            s += cur.val;
+        }
+
+        return answer;
+    }
+}
