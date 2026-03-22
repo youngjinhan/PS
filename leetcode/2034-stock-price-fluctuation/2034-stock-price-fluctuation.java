227c64245d5c4f5d1ec50ab270454de8b57c6e78
youngjinhan <50133281+youngjinhan@users.noreply.github.com>
2026-03-22 22:59:29 +0900

    Time: 74 ms (68.19%), Space: 131.3 MB (51.6%) - LeetHub

--- leetcode/2034-stock-price-fluctuation/2034-stock-price-fluctuation.java
+++ leetcode/2034-stock-price-fluctuation/2034-stock-price-fluctuation.java
@@ -1,3 +1,72 @@
+/** 
+   https://leetcode.com/problems/stock-price-fluctuation/solutions/1514021/java-68-ms-faster-than-100-using-2-prior-dsw9
+ */
+
+class StockRecord {
+    int timestamp;
+    int price;
+    
+    public StockRecord(){}
+    
+    public StockRecord(int t, int p) {
+        timestamp = t;
+        price = p;
+    }
+}
+
+class StockPrice {
+    
+    PriorityQueue<StockRecord> max = new PriorityQueue<>((sr1, sr2) -> (sr2.price - sr1.price));
+    PriorityQueue<StockRecord> min = new PriorityQueue<>((sr1, sr2) -> (sr1.price - sr2.price));
+    StockRecord current_record;
+    Map<Integer, Integer> map = new HashMap<>();
+
+    
+    public StockPrice() {
+        current_record = new StockRecord();
+    }
+    
+    public void update(int timestamp, int price) {
+        if(timestamp >= current_record.timestamp) {
+            current_record.timestamp = timestamp;
+            current_record.price = price;
+        }
+        
+        StockRecord sr = new StockRecord(timestamp, price);
+        max.add(sr);
+        min.add(sr);
+        map.put(timestamp, price);
+    }
+    
+    public int current() {
+        return current_record.price;
+    }
+    
+    public int maximum() {
+        StockRecord sp = max.peek();
+        while(true) {
+            sp = max.peek();
+            if(sp.price != map.get(sp.timestamp))
+                max.poll();
+            else break;
+        }
+        return sp.price;
+    }
+    
+    public int minimum() {
+        StockRecord sp = min.peek();
+         while(true) {
+            sp = min.peek();
+            if(sp.price != map.get(sp.timestamp))
+                min.poll();
+            else break;
+        }
+        return sp.price;
+    }
+}
+
+/** 내 풀이 
+
 class StockPrice {
     private Map<Integer, Integer> timePriceMap;
     private TreeMap<Integer, Integer> priceCountMap;
@@ -38,6 +107,8 @@ class StockPrice {
         return priceCountMap.firstKey();
     }
 }
+
+*/
 
 /**
  * Your StockPrice object will be instantiated and called as such:
